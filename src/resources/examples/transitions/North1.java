fcaca596ed815d32404160e95f3f62e07238bf62
William Baskin <mobile.wbaskin@gmail.com>
2015-05-30 00:03:10 -0400

    Cleaning up warnings

--- src/resources/examples/transitions/North1.java
+++ src/resources/examples/transitions/North1.java
@@ -2,7 +2,6 @@ package resources.examples.transitions;
 
 import resources.AbstractResource;
 import resources.Pair;
-import resources.Transition;
 import resources.examples.environments.Map2D;
 import resources.examples.states.Location2D;
 
