49b4a9ad7fd65968c6b8550e847efc6e0e161106
William Baskin <mobile.wbaskin@gmail.com>
2015-05-29 21:42:41 -0400

    Updates to Algorithm
    includes test case for Comparator to ensure proper sorting. will be
    removed in final version.

--- src/astar/Algorithm.java
+++ src/astar/Algorithm.java
@@ -1,7 +1,77 @@
 package astar;
 
-public class Algorithm {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+import resources.*;
+
+public class Algorithm <S extends State, T extends Transition, E extends Environment> {//, H extends Heuristic> {
 	
+	//H heuristic;
 	
+	//public Algorithm(H heuristic) {
+	//	this.heuristic = heuristic;
+	//}
+	
+	public Stack<Pair<S,T>> search(S initial, S goal, T[] moves, E env) {
+		PriorityQueue<S> frontier = new PriorityQueue<S>();
+		
+		return null;
+	}
+	
+	//
+	private double f(S current, S goal) {
+		// TODO fix
+		return 0.0;
+	}
+	
+	private class AstarComparator implements Comparator<S> {
+		S goal;
+    	public AstarComparator(S goal)
+    	{
+    		this.goal = goal;
+    	}
+		@Override
+		public int compare(S arg0, S arg1) {
+			if (f(arg0, goal) - f(arg1, goal) == 0.0 ) {
+				return 0;
+			}
+			else if (f(arg0, goal) - f(arg1, goal) < 0.0 ) {
+				return -1;
+			}
+			else {
+				return 1;
+			}
+		}
+	}
+	/*
+	public static class MyComparator implements Comparator<Integer> {
+		@Override
+		public int compare(Integer one, Integer two) {
+			if (one - two == 0.0 ) {
+				return 0;
+			}
+			else if (one - two < 0.0 ) {
+				return -1;
+			}
+			else {
+				return 1;
+			}
+		}
+	}
+	*/
+	
+	public static void main(String[] args) {
+		/*
+		PriorityQueue<Integer> pqi = new PriorityQueue<Integer>(new MyComparator());
+		pqi.add(1);
+		pqi.add(3);
+		pqi.add(2);
+		System.out.println("1: "+pqi.poll());
+		System.out.println("2: "+pqi.poll());
+		System.out.println("3: "+pqi.poll());
+		*/
+	}
 	
 }
